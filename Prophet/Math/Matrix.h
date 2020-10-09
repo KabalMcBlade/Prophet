886db0b0249d5fb78c5f4723628df4a4c8d329d5
KabalMcBlade <mcondokmb@gmail.com>
2020-10-09 11:41:38 +0100

    set std cpp lang level 17 (for implmenentation of more complicated meta template pattern) Matrix work in progress

--- Prophet/Math/Matrix.h
+++ Prophet/Math/Matrix.h
@@ -9,7 +9,19 @@
 
 NAMESPACE_BEGIN
 
-template<typename T, int ROWS, int COLUMNS>
+// temporally here, have to move somewhere else!
+static constexpr uint32 Ceil(float num)
+{
+	uint32 inum = static_cast<uint32>(num);
+	if (num == static_cast<float>(inum))
+	{
+		return inum;
+	}
+	return inum + (num > 0 ? 1 : 0);
+};
+
+
+template<typename T, uint32 ROWS, uint32 COLUMNS>
 class Matrix : public MathAligned<MEM_ALIGN>
 {
 	static_assert(std::numeric_limits<T>::is_integer || std::is_floating_point<T>::value, "Matrix can be only instantiated with integer, float or double");
@@ -17,15 +29,32 @@ class Matrix : public MathAligned<MEM_ALIGN>
 public:
 	INLINE Matrix()
 	{
-		m_rows = ROWS;
-		m_columns = COLUMNS;
-
 
 	}
 
+	INLINE void SetRandomUniformDistribution()
+	{
+#if ARCH & ARCH_AVX512_FLAG
+		// 512
+#elif (ARCH & ARCH_AVX_FLAG)
+		// 256
+#else
+		// 128
+#endif
+	}
+
+	INLINE uint32 GetRowsCount() const { return ROWS; };
+	INLINE uint32 GetColumnsCount() const { return COLUMNS; };
+
 private:
-	int m_rows;
-	int m_columns;
+	using Type = typename std::conditional<std::is_same<int, T>::value, __int,
+		typename std::conditional<std::is_same<float, T>::value, __float,
+			__double
+		>::type
+	>::type;
+
+	Type m_buffer[Ceil(ROWS / SCALAR_COUNT) % SCALAR_COUNT][Ceil(COLUMNS / SCALAR_COUNT) % SCALAR_COUNT];
+	//ALIGNED T m_buffer[ROWS][COLUMNS];
 };
 
 NAMESPACE_END
